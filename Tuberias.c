28a7db036f1bf982ee9df432593f483f0255aa85
dario-matamo <you@example.com>
2019-11-09 12:27:51 -0600

    Funcion que crea el espacio en memoria para la matriz

--- Tuberias.c
+++ Tuberias.c
@@ -1,1 +1,10 @@
 //Aqui se hace el desarrollo de las funciones
+#include <stdlib.h>
+int **crearMatriz(int filas, int columnas){
+	int **matriz;
+	matriz = (int**)malloc(sizeof(int*)*filas);
+	for(int i=0;i<filas;i++){
+		matriz[i] = (int*)malloc(sizeof(int)*columnas);
+	}
+	return matriz;
+}
