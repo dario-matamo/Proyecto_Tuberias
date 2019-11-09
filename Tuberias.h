cce89da96f794ec2fa6eb5552378fed025b820ee
dario-matamo <you@example.com>
2019-11-09 12:24:49 -0600

    Variables necesarias para la matriz, funcion de creacion de matriz y variables de entrada y salidas

--- Tuberias.h
+++ Tuberias.h
@@ -3,3 +3,6 @@ int columnas = 0;
 int entradas = 0;
 int salidas = 0;
 int **tuberias;
+
+/**Funcion que sirve para crear la matriz, NECESITA CAMBIO DE TIPO AL TIPO DE LA MATRIZ*/
+int **crearMatriz(int filas, int columnas);
