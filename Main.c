6b89ecd1d64985e516c6c46f2ac05d6b15ba53ca
dario-matamo <you@example.com>
2019-11-12 02:18:09 -0600

    Adding testing for main

--- Main.c
+++ Main.c
@@ -1,1 +1,11 @@
-//Este es el main
+#include <stdio.h>
+#include "Pipes.h"
+int main(){
+	
+	pipes** pipesMatrix;
+	rows = 4;
+	columns = 4;
+	pipesMatrix = buildMatrix(pipesMatrix,rows,columns);
+    	
+	return 0;
+}
