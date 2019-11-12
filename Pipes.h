eb1a791d87b38428098e551bbadde20d9e0b03e3
dario-matamo <you@example.com>
2019-11-12 01:44:33 -0600

    Se anade el struct con las piezas de la tuberia

---
+++ Pipes.h
@@ -0,0 +1,34 @@
+/**Struct that contains every pipe type*/
+typedef struct{
+    const char empty [4];
+
+    const char plugUp[4];
+    const char plugDown[4];
+    const char plugLeft[4];
+    const char plugRight[4];
+
+    const char  twDownLeft[4];
+    const char  twDownRight[4];
+    const char  twUpLeft[4];
+    const char  twUpRight[4];
+
+    const char horizontal[4];
+    const char vertical[4];
+
+    const char trRightDownLeft[4];
+    const char trUpLeftDown[4];
+    const char trLeftUpRight[4];
+    const char trUpRightDown[4];
+
+    const char trAll[4];
+} pipes;
+/** Variables to catch from the file*/
+int rows = 0;
+int columns = 0;
+int entries = 0;
+int exits = 0;
+
+pipes  **pipesMatrix;
+
+
+
--- Tuberias.h
+++
@@ -1,8 +0,0 @@
-int filas = 0;
-int columnas = 0;
-int entradas = 0;
-int salidas = 0;
-int **tuberias;
-
-/**Funcion que sirve para crear la matriz, NECESITA CAMBIO DE TIPO AL TIPO DE LA MATRIZ*/
-int **crearMatriz(int filas, int columnas);
