f88325f6776bd9b8c7c675bc8375c2a500b98495
dario-matamo <you@example.com>
2019-11-13 11:06:22 -0600

    Added implementation of function to specify a type of pipe

--- Pipes.c
+++ Pipes.c
@@ -17,3 +17,122 @@ pipe **buildMatrix(pipe **pipesMatrix, int rows, int columns){
 void readData(int *ptrRows, int *ptrColumns, int *ptrEntries,int *ptrExits){
 	scanf("%d %d %d %d",ptrRows,ptrColumns,ptrEntries,ptrExits);
 }
+
+/**Function that specifies what type of pipe a hexadecimal code represents*/
+pipe specifyPipe(char code){
+pipe pipeSpecifier;
+    if(code == '0'){
+        pipeSpecifier.up = '0';
+        pipeSpecifier.down = '0';
+        pipeSpecifier.left = '0';
+        pipeSpecifier.right = '0';
+    }else{
+        if(code == '1'){
+            pipeSpecifier.up = '0';
+            pipeSpecifier.down = '0';
+            pipeSpecifier.left = '1';
+            pipeSpecifier.right = '0';
+        }else{
+            if(code == '2'){
+                pipeSpecifier.up = '0';
+                pipeSpecifier.down = '1';
+                pipeSpecifier.left = '0';
+                pipeSpecifier.right = '0';
+            }else{
+                if(code == '3'){
+                    pipeSpecifier.up = '0';
+                    pipeSpecifier.down = '1';
+                    pipeSpecifier.left = '1';
+                    pipeSpecifier.right = '0';
+                }else{
+                    if(code == '4'){
+                        pipeSpecifier.up = '0';
+                        pipeSpecifier.down = '0';
+                        pipeSpecifier.left = '0';
+                        pipeSpecifier.right = '1';
+                    }else{
+                        if(code == '5'){
+                            pipeSpecifier.up = '0';
+                            pipeSpecifier.down = '0';
+                            pipeSpecifier.left = '1';
+                            pipeSpecifier.right = '1';
+                        }else{
+                            if(code == '6'){
+                                pipeSpecifier.up = '0';
+                                pipeSpecifier.down = '1';
+                                pipeSpecifier.left = '0';
+                                pipeSpecifier.right = '1';
+                            }else{
+                                if(code == '7'){
+                                    pipeSpecifier.up = '0';
+                                    pipeSpecifier.down = '1';
+                                    pipeSpecifier.left = '1';
+                                    pipeSpecifier.right = '1';
+                                }else{
+                                    if(code == '8'){
+                                        pipeSpecifier.up = '1';
+                                        pipeSpecifier.down = '0';
+                                        pipeSpecifier.left = '0';
+                                        pipeSpecifier.right = '0';
+                                    }else{
+                                        if(code == '9'){
+                                            pipeSpecifier.up = '1';
+                                            pipeSpecifier.down = '0';
+                                            pipeSpecifier.left = '1';
+                                            pipeSpecifier.right = '0';
+                                        }else{
+                                            if(code == 'A'){
+                                                pipeSpecifier.up = '1';
+                                                pipeSpecifier.down = '1';
+                                                pipeSpecifier.left = '0';
+                                                pipeSpecifier.right = '0';
+                                            }else{
+                                                if(code == 'B'){
+                                                    pipeSpecifier.up = '1';
+                                                    pipeSpecifier.down = '1';
+                                                    pipeSpecifier.left = '1';
+                                                    pipeSpecifier.right = '0';
+                                                }else{
+                                                    if(code == 'C'){
+                                                            pipeSpecifier.up = '1';
+                                                            pipeSpecifier.down = '0';
+                                                            pipeSpecifier.left = '0';
+                                                            pipeSpecifier.right = '1';
+                                                    }else{
+                                                        if(code == 'D'){
+                                                            pipeSpecifier.up = '1';
+                                                            pipeSpecifier.down = '0';
+                                                            pipeSpecifier.left = '1';
+                                                            pipeSpecifier.right = '1';
+                                                        }else{
+                                                            if(code == 'E'){
+                                                                pipeSpecifier.up = '1';
+                                                                pipeSpecifier.down = '1';
+                                                                pipeSpecifier.left = '0';
+                                                                pipeSpecifier.right = '1';
+                                                            }else{
+                                                                if(code == 'F'){
+                                                                    pipeSpecifier.up = '1';
+                                                                    pipeSpecifier.down = '1';
+                                                                    pipeSpecifier.left = '1';
+                                                                    pipeSpecifier.right = '1';
+                                                                }else{
+                                                                   printf("ERROR");
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }    
+                }   
+            }
+        }
+    }
+    return pipeSpecifier;
+}
