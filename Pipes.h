17d5c16f0513abc4a5d837934624350566042f68
dario-matamo <56232927+dario-matamo@users.noreply.github.com>
2019-11-17 20:36:36 -0600

    Declarations of functions to find leaks
    Added functions to make possible the method of finding leaks on the matrix.

--- Pipes.h
+++ Pipes.h
@@ -1,3 +1,9 @@
+/**
+*@brief File that contains all the declarations of variables and methods
+*@author Dario Matamoros Vega
+*/
+#include<stdlib.h>
+#include<stdio.h>
 /**Struct that contains every conection that can have a pipe*/
 typedef struct{
     char up;
@@ -6,7 +12,7 @@ typedef struct{
     char right;
 } pipe;
 
-/** Variables to catch from the file and needed for functions*/
+/**Variables to catch from the file and needed for functions*/
 
 int rows;
 int columns;
@@ -15,11 +21,39 @@ int exits;
 
 pipe **pipeMatrix;
 
-/**Function to read and change values of rows, columns, entries and exits*/
+/**
+ * @brief Function to read and change values of rows, columns, entries and exits
+ * @param ptrRows Pointer that points to variable that contain the numbers of rows in the matrix
+ * @param ptrColumns Pointer that points to variable that contain the numbers of columns in the matrix
+ * @param ptrEntries Pointer that points to variable that contain the numbers of entries
+ * @param Pointer that points to variable that contain the numbers of exits
+*/
 void readData(int *ptrRows, int *ptrColumns, int *ptrEntries, int *ptrExits);
-/**Function to build a matrix of pipes*/
+/** 
+ * @brief Function to build a matrix of pipes
+ * @param pipesMatrix Matrix made of pipes 
+ * @param rows Number of rows in the matrix
+ * @param columns Number of columns in the matrix
+ * @return pipe The function creates the memory necesary for the matrix
+*/
 pipe **buildMatrix(pipe **pipesMatrix, int rows, int columns);
-/**Function that specifies what type of pipe a hexadecimal code represents*/
+/**
+ * @brief Function that specifies what type of pipe a hexadecimal code represents
+ * @param code Caracter that represents a type of pipe
+ * @return pipe The function returns a pipe depending on the code
+*/
 pipe specifyPipe(char code);
-/**Function to fill the matrix with the pipes types*/
+/**
+ * @brief Function to fill the matrix with the pipes types
+ * @param pipesMatrix Matrix made of pipes 
+ * @param rows Number of rows in the matrix
+ * @param columns Number of columns in the matrix 
+*/
 void fillMatrix(pipe **pipesMatrix,int rows,int columns);
+/**
+ * @brief Function to get where are leaks
+ * @param pipesMatrix Matrix made of pipes 
+ * @param rows Number of rows in the matrix
+ * @param columns Number of columns in the matrix 
+ */
+void findLeaks(pipe **pipesMatrix,int rows, int columns);
